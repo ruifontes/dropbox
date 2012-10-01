d5367934d63bff4a56a79a24947bba85de3788eb
Patrick Zajda <patrick@zajda.fr>
2012-10-01 13:14:52 +0000

    First commit

---
+++ appModules/dropbox.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+import appModuleHandler,ctypes,NVDAObjects,controlTypes,addonHandler
+from ui import message
+from winUser  import  mouse_event,MOUSEEVENTF_LEFTUP,MOUSEEVENTF_LEFTDOWN,isWindowVisible,setCursorPos,sendMessage,setFocus,getWindowText,isWindowEnabled
+from api import getFocusObject
+addonHandler.initTranslation()
+listPageTab=(_("General"),_("Account"),_("Bandwidth"),_("Proxies"),_("Advanced"))
+def getHandlePageTab():
+
+	FindWindowExA=ctypes.windll.user32.FindWindowExA
+	h=int ()
+	for i in range (4):
+		h=FindWindowExA(h,None,"wxWindowClassNR",None)
+	return h
+def getPageTabActive ():
+	firstChild,next=5,2
+	GetWindow=ctypes.windll.user32.GetWindow
+	h=getHandlePageTab()
+	h=ctypes.windll.user32.GetParent(h)
+	h=GetWindow(GetWindow(h,next),next)
+	h=GetWindow(h,firstChild)
+	i=1
+	while isWindowVisible (h)==False:
+		h=GetWindow(h,next)
+		i+=1
+		if not h :
+			return
+	return listPageTab[i-1]
+
+def changePageTab (sens):
+	#index =getPageTabActive ()[-1]
+	index=listPageTab.index (getPageTabActive())
+	if sens =="next":
+		index+=1
+	else:
+		index-=1
+	if index >=5:
+		index =0
+	elif index<=-1:
+		index=4
+
+	(x,y,l,h) = NVDAObjects.IAccessible.getNVDAObjectFromEvent(getHandlePageTab(),-4,0).IAccessibleObject.accLocation (0)
+	x=x+(1,3,5,7,9)[index]*l/10
+	y=y+h/2
+	setCursorPos (x,y)
+	mouse_event(MOUSEEVENTF_LEFTDOWN,0,0,None,None)
+	mouse_event (MOUSEEVENTF_LEFTUP,0,0,None,None)
+
+	#annonce le nouvel onglet
+	message (listPageTab[index])
+
+
+
+class AppModule(appModuleHandler.AppModule):
+	def script_clickButtonCancel (self,gesture):
+		h=getFocusObject().windowHandle
+		GetParent =ctypes.windll.user32.GetParent
+		while GetParent(h) and isWindowEnabled(GetParent (h)):
+			h=GetParent(h)
+
+		FindWindowExA=ctypes.windll.user32.FindWindowExA
+		while FindWindowExA (h,None,"Button",_("Cancel"))==False :
+			h=ctypes.windll.user32.GetWindow (h,5)
+			if not h :
+				gesture.send()
+				return
+		h=FindWindowExA(h,None,"Button",_("Cancel"))
+		NVDAObjects.IAccessible.getNVDAObjectFromEvent (h,-4,0).IAccessibleObject.accDoDefaultAction (0)
+
+	def event_gainFocus(self, obj, nextHandler):
+		if obj.windowHandle ==getHandlePageTab():
+			obj.name=getPageTabActive()
+			obj.role=controlTypes.ROLE_TABCONTROL
+
+
+		nextHandler()
+
+
+	def script_sayPageTabActive(self,gesture,):
+		message (getPageTabActive())
+	def script_priorPageTab(self,gesture):
+		changePageTab("prior")
+	def script_nextPageTab (self,gesture):
+		changePageTab("next")
+
+	__gestures={
+"kb:control+tab":"nextPageTab",
+"kb:control+shift+tab":"priorPageTab",
+"kb:control+alt+t":"sayPageTabActive",
+"kb:escape":"clickButtonCancel"
+}
---
+++ globalPlugins/SayStatedropbox.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+# Dropbox announcement Global Plugin for NVDA
+# Authors: Filaos, Patrick ZAJDA
+# Shortcut: NVDA+Shift+D
+
+import globalPluginHandler,IAccessibleHandler,addonHandler
+import scriptHandler
+from ui import message
+import ctypes
+import NVDAObjects
+from api import getFocusObject
+import winUser
+
+# We initialize translation support
+addonHandler.initTranslation()
+
+def rightMouseButton(o):
+	#simulation du bouton droit de la souris
+	#récupération de l'identifiant de l'objet ,car on ne pourra pas agir directement sur l'objet 
+	IDChild =o.IAccessibleChildID
+	#on remonte d'un niveau pour récupérer les coordonnées de l'objet ceci est une particularité
+	o=o.parent
+	#on récupère les coordonnées de l'objet enfant de o dont l'identifiant est IDChild
+	(x,y,l,h)=o.IAccessibleObject.accLocation(IDChild)
+	#on calcul le centre de l'objet enfant
+	x,y=int (x+l/2),int (y+h/2)
+	#on déplace le curseur
+	winUser.setCursorPos (x,y)
+	#on simule l'appuie sur le click droit de la souris 
+	winUser.mouse_event(winUser.MOUSEEVENTF_RIGHTDOWN,0,0,None,None)
+	#on simule le relâchement du bouton droit de la souris
+	winUser.mouse_event(winUser.MOUSEEVENTF_RIGHTUP,0,0,None,None)
+
+class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+	def script_announceDropbox(self, gesture):
+		l=("shell_TrayWnd","TrayNotifyWnd","SysPager","ToolbarWindow32")
+		h,FindWindowExA =0,ctypes.windll.user32.FindWindowExA
+		for element in l:
+			h=FindWindowExA(h,0,element,0)
+
+		o=NVDAObjects.IAccessible.getNVDAObjectFromEvent(h,-4,1)
+		name=o.name
+		while (name[0:7].lower()).find ("dropbox")==-1:
+			o=o.next
+			if not o :
+				message(_("drop box not found"))
+				return
+			else:
+				name=o.name
+
+
+		name =name.split ()
+		#stockage du nombre de fois que le script a été appelé
+		isSameScript =scriptHandler.getLastScriptRepeatCount()
+		if isSameScript ==0 :
+			del (name[1])
+			name=" ".join (name)
+		elif isSameScript ==1:
+			name = " ".join(name[0:2])
+		else:
+			objFocused = getFocusObject()
+			currentProcess = objFocused.appModule.appName.lower()
+			if currentProcess != u'dropbox' and objFocused.windowClassName.lower() != u'#32768':
+				rightMouseButton (o)
+			return
+		message (name)
+	# Documentation
+	script_announceDropbox.__doc__ = _("Announce Dropbox' status")
+
+	__gestures={
+		"kb:NVDA+shift+d": "announceDropbox",
+	}
+
