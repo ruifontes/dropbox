2665c34d91be2da48aa165a1b18c4f9d07eef831
Patrick Zajda <patrick@zajda.fr>
2012-10-03 15:57:45 +0000

    Added my e-mail address. A little modification here only to test the web hook :)

--- addon/appModules/dropbox.py
+++ addon/appModules/dropbox.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Dropbox appModule for NVDA
 # Make preferences tabs accessible.
-# Authors: mainly Filaos, Patrick ZAJDA to make it translatable
+# Authors: mainly Filaos, Patrick ZAJDA <patrick@zajda.fr> to make it translatable
 
 import appModuleHandler,ctypes,NVDAObjects,controlTypes,addonHandler
 from ui import message
