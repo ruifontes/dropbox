42a8396a965af27bb20de5678d2afcd8fc0b0ac1
Patrick Zajda <patrick@zajda.fr>
2012-10-05 10:36:56 +0000

    AppModule: Removed/reorganized imports. Removed getHandlePageTab, now the handle is a property of the class All functions conserning tabs have another parameter: the handle of the page tab Fixed problem with Ctrl+Tab and Ctrl+Shift+Tab which worked randomly before.

--- addon/appModules/dropbox.py
+++ addon/appModules/dropbox.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 # Dropbox appModule for NVDA
 # Make preferences tabs accessible.
-# Authors: mainly Filaos, Patrick ZAJDA <patrick@zajda.fr> to make it translatable
+# Authors: Filaos, Patrick ZAJDA <patrick@zajda.fr>
 
-import appModuleHandler,NVDAObjects,controlTypes,addonHandler,winUser
+import addonHandler,appModuleHandler,NVDAObjects,controlTypes,winUser
 
 from ui import message
-#from winUser  import  mouse_event,MOUSEEVENTF_LEFTUP,MOUSEEVENTF_LEFTDOWN,isWindowVisible,setCursorPos,setFocus,getWindowText,isWindowEnabled
 from api import getFocusObject
 
 # We initialize translations
@@ -16,19 +15,11 @@ addonHandler.initTranslation()
 # Tab texts are translatables
 listPageTab=(_("General"),_("Account"),_("Bandwidth"),_("Proxies"),_("Advanced"))
 
-def getHandlePageTab():
-	""" Get the tab pannel handle """
-	FindWindowExA=winUser.user32.FindWindowExA
-	h=int ()
-	for i in range (4):
-		h=FindWindowExA(h,None,"wxWindowClassNR",None)
-	return h
-
-def getPageTabActive ():
-	""" Get the handle of the active tab """
+def getPageTabActive (h):
+	""" Get the handle of the active tab
+	@param h the handle of the tab page """
 	firstChild,next=5,2
 	GetWindow=winUser.getWindow
-	h=getHandlePageTab()
 	h=winUser.user32.GetParent(h)
 	h=GetWindow(GetWindow(h,next),next)
 	h=GetWindow(h,firstChild)
@@ -40,11 +31,12 @@ def getPageTabActive ():
 			return
 	return listPageTab[i-1]
 
-def changePageTab (sens):
+def changePageTab (h,sens):
 	""" Change the active tab
+	@param h the handle of tab page
 	@param sens : string, next or prior """
 	#index =getPageTabActive ()[-1]
-	index=listPageTab.index (getPageTabActive())
+	index=listPageTab.index (getPageTabActive(h))
 	if sens =="next":
 		index+=1
 	else:
@@ -54,7 +46,7 @@ def changePageTab (sens):
 	elif index<=-1:
 		index=4
 
-	(x,y,l,h) = NVDAObjects.IAccessible.getNVDAObjectFromEvent(getHandlePageTab(),-4,0).IAccessibleObject.accLocation (0)
+	(x,y,l,h) = NVDAObjects.IAccessible.getNVDAObjectFromEvent(h,-4,0).IAccessibleObject.accLocation (0)
 	x=x+(1,3,5,7,9)[index]*l/10
 	y=y+h/2
 	winUser.setCursorPos (x,y)
@@ -64,9 +56,10 @@ def changePageTab (sens):
 	#announces the new tab
 	message (listPageTab[index])
 
-
-
 class AppModule(appModuleHandler.AppModule):
+	# The tab page handle
+	tabPageHandle = 0
+
 	def script_clickButtonCancel (self,gesture):
 		h=getFocusObject().windowHandle
 		GetParent =winUser.user32.GetParent
@@ -83,18 +76,19 @@ class AppModule(appModuleHandler.AppModule):
 		NVDAObjects.IAccessible.getNVDAObjectFromEvent (h,-4,0).IAccessibleObject.accDoDefaultAction (0)
 
 	def event_NVDAObject_init(self, obj):
-		if obj.windowHandle ==getHandlePageTab() or obj.name == u'buttonPanel':
-			obj.name=getPageTabActive()
+		if obj.name == u'buttonPanel':
 			obj.role=controlTypes.ROLE_TABCONTROL
+			self.tabPageHandle = obj.windowHandle
+			obj.name=getPageTabActive(self.tabPageHandle)
 
 	def script_sayPageTabActive(self,gesture,):
-		message (getPageTabActive())
+		message (getPageTabActive(self.tabPageHandle))
 
 	def script_priorPageTab(self,gesture):
-		changePageTab("prior")
+		changePageTab(self.tabPageHandle,"prior")
 
 	def script_nextPageTab (self,gesture):
-		changePageTab("next")
+		changePageTab(self.tabPageHandle,"next")
 
 	__gestures={
 "kb:control+tab":"nextPageTab",
