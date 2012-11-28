c85546981ca0bc8da7aa64c7d5f2c2e97a929c5b
Patrick ZAJDA <patrick@zajda.fr>
2012-11-28 16:14:11 +0100

    Implements the new add-on template. Modifies .gitignore to add *.nvda-addon and some others files. Modifies sconstruct to solve a problem with translatted manifest files.

--- buildVars.py
+++ buildVars.py
@@ -1,6 +1,31 @@
 # Build customizations
 # Change this file instead of sconstruct, whenever possible.
 
+# Full getext (please don't change)
+_ = lambda x : x
+
+# Add-on information variables
+addon_info = {
+# add-on Name
+	"addon-name" : "dropbox",
+	# Add-on description
+	# TRANSLATORS: Summary for this add-on to be shown on installation and add-on informaiton.
+	"addon-summary" : _("Announce Dropbox state and make preferences tabs accessible."),
+	# Add-on description
+	# Translators: Long description to be shown for this add-on on installation and add-on information
+	"addon-description" : _("""Announces Dropbox status, version or open the Dropbox systray menu.
+Shortcut: NVDA+Shift+D
+Page tabs are also working in the preferences dialog with Ctrl+tab and Shift+Ctrl+Tab.
+Ctrl+Alt+T announce the active tab.
+You can activate cancel by pressing the escape key."""),
+	# version
+	"addon-version" : "3.0",
+	# Author(s)
+	"addon-author" : "Patrick ZAJDA <patrick@zajda.fr>, Filaos and other contributors",
+# URL for the add-on documentation support
+"addon-url" : None
+}
+
 import os.path
 
 # Define the python files that are the sources of your add-on.
@@ -9,5 +34,5 @@ pythonSources = [os.path.join("addon", "appModules", "*.py"),
     os.path.join("addon", "globalPlugins", "*.py")]
 
 # Files that contain strings for translation. Usually your python sources
-i18nSources = pythonSources 
+i18nSources = pythonSources + ["buildVars.py"]
 
