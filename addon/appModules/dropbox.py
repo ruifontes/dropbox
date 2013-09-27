f016079611ae46c884d6424acd5a3cf0ac095bc2
David Parduhn <xkill85@gmx.net>
2013-09-27 23:25:36 +0200

    AppModule: add accesssibility improvements for Dropbox Metro App.
    Signed-off-by: Patrick ZAJDA <patrick@zajda.fr>

--- addon/appModules/dropbox.py
+++ addon/appModules/dropbox.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 # Dropbox appModule for NVDA
 # Make preferences tabs accessible and allow pressing escape to quit the preferences dialog.
-# Copyright (C) 2012 Filaos, Patrick ZAJDA <patrick@zajda.fr> and other contributors
+# Also improves accessibility of the Dropbox Metro app for Windows 8
+# Copyright (C) 2013 Filaos, Patrick ZAJDA <patrick@zajda.fr> and other contributors
+# Windows 8 compatibility contributed by David Parduhn <xkill85@gmx.net>
 # This file is covered by the GNU General Public License.
 # You can read the licence by clicking Help->Licence in the NVDA menu
 # or by visiting http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
@@ -12,6 +14,7 @@ import NVDAObjects
 import controlTypes
 import winUser
 import ui
+from NVDAObjects import UIA
 
 # We initialize translations
 addonHandler.initTranslation()
@@ -70,11 +73,26 @@ def changePageTab (h,sens):
 	#announces the new tab
 	ui.message(listPageTab[index])
 
+# Class for Dropbox items in the metro app
+class dropboxitem(UIA.ListItem):
+	def _get_name(self):
+		obj = self.firstChild
+		s = obj.name
+		while (obj != self.lastChild):
+			if obj.role == controlTypes.ROLE_STATICTEXT:
+				s = s +obj.name
+			obj = obj.next
+		return s
+
 class AppModule(appModuleHandler.AppModule):
 	def event_NVDAObject_init(self, obj):
 		if obj.name == u'buttonPanel':
 			obj.role=controlTypes.ROLE_TABCONTROL
 			obj.name=getPageTabActive(obj.windowHandle)
+
+	def chooseNVDAObjectOverlayClasses(self, obj, CLSList):
+		if obj.role == controlTypes.ROLE_LISTITEM:
+			CLSList.insert(0, dropboxitem)
 
 	def script_clickButtonCancel (self,gesture):
 		# Translators: the title of the dropbox preferences dialog, it is important to have the same capitalization/spelling as in the dropbox gui. If Dropbox hasn't been translated to your language yet, leave this blank or write the original text.
