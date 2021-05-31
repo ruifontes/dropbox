3d6e087bef7316f02640617b15c20c36333e63a7
Patrick ZAJDA <patrick@zajda.fr>
2021-05-31 18:09:22 +0200

    Still fighting against lines too long

--- addon/globalPlugins/SayStatedropbox.py
+++ addon/globalPlugins/SayStatedropbox.py
@@ -76,8 +76,8 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 			objFocused = api.getFocusObject()
 			currentProcess = objFocused.appModule.appName.lower()
 			if (
-			    (currentProcess.lower() == u'dropbox' and objFocused.windowClassName.lower() == u'#32768')
-			    and objFocused.role in ({controlTypes.ROLE_POPUPMENU, controlTypes.ROLE_MENUITEM})
+				    (currentProcess.lower() == u'dropbox' and objFocused.windowClassName.lower() == u'#32768')
+				and objFocused.role in ({controlTypes.ROLE_POPUPMENU, controlTypes.ROLE_MENUITEM})
 			):
 				return
 			else:
@@ -87,7 +87,8 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 					# Translators: the message reported when it is not possible to activate Dropbox context menu.
 					ui.message(_("Unable to activate Dropbox context menu"))
 	# Translators: message presented when user performes input help for the Shift+NVDA+D script
-	script_announceDropbox.__doc__ = _("If pressed once, announces Dropbox status. If pressed twice, open the Dropbox context menu by clicking on its systray icon")
+	script_announceDropbox.__doc__ =\
+	_("If pressed once, announces Dropbox status. If pressed twice, open the Dropbox context menu by clicking on its systray icon")
 
 	__gestures = {
 		"kb:NVDA+alt+d": "announceDropbox",
